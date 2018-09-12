fdfa80af5c2f7152b63ba3744c07c2e8b8697076
shencang <2630610009@qq.com>
2018-09-12 21:24:12 +0800

    N7终于弄好了

--- src/N6交叉队列.java
+++ src/N6交叉队列.java
@@ -13,10 +13,10 @@ public class N6交叉队列 {
         reslut[0][0] = true;
         for (int i = 0; i < lengthA + 1; i++) {
             for (int j = 0; j < lengthB + 1; j++) {
-                if ((i - 1) >= 0 && reslut[i - 1][j] && array[0].charAt(i - 1) == array[2].charAt(i - 1 + j)) { // dp[i-1][j]=true表示s3[1, i-1+j]由s1[1,i-1]和s2[1,j]交错组成,如果s1.charAt(i-1)与s3.charAt(i-1+j)相等，那么很显然dp[i][j]也为true
+                if ((i - 1) >= 0 && reslut[i - 1][j] && array[0].charAt(i - 1) == array[2].charAt(i - 1 + j)) { // reslut[i-1][j]=true表示s3[1, i-1+j]由s1[1,i-1]和s2[1,j]交错组成,如果s1.charAt(i-1)与s3.charAt(i-1+j)相等，那么很显然reslut[i][j]也为true
                     reslut[i][j] = true;
                 }
-                if ((j - 1) >= 0 && reslut[i][j - 1] && array[1].charAt(j - 1) == array[2].charAt(i + j - 1)) { // dp[i][j-1]=true表示s3[1, i+j-1]由s1[1,i]和s2[1,j-1]交错组成,如果s2.charAt(j-1)与s3.charAt(i+j-1)相等，那么很显然dp[i][j]也为true
+                if ((j - 1) >= 0 && reslut[i][j - 1] && array[1].charAt(j - 1) == array[2].charAt(i + j - 1)) { // reslut[i][j-1]=true表示s3[1, i+j-1]由s1[1,i]和s2[1,j-1]交错组成,如果s2.charAt(j-1)与s3.charAt(i+j-1)相等，那么很显然reslut[i][j]也为true
                     reslut[i][j] = true;
                 } // 除以上两种情况外，result[i][j]为false
             }
