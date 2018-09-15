d59d24f95d2e6020df4343c0440cfd5f62b74af8
shencang <2630610009@qq.com>
2018-09-15 22:00:31 +0800

    N11写一部分

--- src/N11构建短字符串.java
+++ src/N11构建短字符串.java
@@ -1,2 +1,21 @@
 public class N11构建短字符串 {
+
+    private static String solution(String line) {
+
+        String[] array = line.split(" ");
+        String result = "";
+
+
+        //   result = Integer.toString();
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(solution("uak areuok"));
+
+
+    }
 }
