3fc216008a113939b8e367473c0d0a3c8c59280a
shencang <2630610009@qq.com>
2018-09-16 22:11:32 +0800

    N12部分

--- src/N12找出可能的合的组合.java
+++ src/N12找出可能的合的组合.java
@@ -1,2 +1,22 @@
 public class N12找出可能的合的组合 {
+
+    private static String solution(String line) {
+
+        String[] array = line.split(" ");
+        String result = "";
+        String s1 = array[0];
+        String s2 = array[1];
+        String[] sa = s1.split(",");
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(solution("uak areuok"));
+
+
+    }
 }
