1a3f72993631ac9d97e96b91f366c7a13d23a01f
shencang <2630610009@qq.com>
2018-09-16 21:59:04 +0800

    N11

--- src/N11构建短字符串.java
+++ src/N11构建短字符串.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class N11构建短字符串 {
 
     private static String solution(String line) {
@@ -8,58 +6,41 @@ public class N11构建短字符串 {
         String result = "";
         String s1 = array[0];
         String s2 = array[1];
-        int num = 0;
-        s1 = sort(s1);
-        s2 = sort(s2);
         char[] ch1 = s1.toCharArray();
         char[] ch2 = s2.toCharArray();
-        //  System.out.println(s1+" "+s2);
-        for (int i = 0; i < ch2.length; i++) {
-            for (int j = 0; j < ch1.length; j++) {
-                if (ch2[i] == ch1[j]) {
-                    num++;
-                    //   System.out.println(num);
-                }
-            }
-        }
-        if (num == ch1.length) {
+
+        if (find(ch1, ch2)) {
             return "true";
         } else {
             return "false";
         }
     }
 
+    public static boolean find(char[] a, char[] b) {
 
-    private static String sort(String str) {
-        //把字符串转化成字符数组
-        char[] chs = stringToCharArray(str);
+        for (int i = 0; i < a.length; i++) {
+            int c = 0;
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    b[j] = '!';
+                    c = 1;
+                    break;
+                }
+            }
+            if (c == 0) {
+                return false;
+            }
 
-        //对字符数组进行排序
-        charSort(chs);
-
-        //把字符数组转化成字符串
-        String s = new String(chs);
-
-        return s;
+        }
+        return true;
     }
 
-    private static void charSort(char[] chs) {
-
-        Arrays.sort(chs);
-
-    }
-
-    private static char[] stringToCharArray(String str) {
-
-
-        return str.toCharArray();
-    }
 
 
     public static void main(String[] args) {
 
 
-        System.out.println(solution("aa ab"));
+        System.out.println(solution("uak areuok"));
 
 
     }
