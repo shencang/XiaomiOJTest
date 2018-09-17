5040dde8831188b4c756010e0affa0eed2ee26f4
shencang <2630610009@qq.com>
2018-09-17 20:52:03 +0800

    N12

--- src/N12找出可能的合的组合.java
+++ src/N12找出可能的合的组合.java
@@ -6,17 +6,36 @@ public class N12找出可能的合的组合 {
         String result = "";
         String s1 = array[0];
         String s2 = array[1];
+        int n = Integer.parseInt(s2);
         String[] sa = s1.split(",");
 
-        return "";
+        return Integer.toString(get_num(sa, n));
+
+
+        //return "";
+    }
+
+    static int get_num(String[] na, int n) {
+        int sum = 0;
+        for (int i = 0; i < na.length; i++) {
+            if (n - Integer.parseInt(na[i]) == 0) {
+                sum++;
+                break;
+            }
+            if (n - Integer.parseInt(na[i]) > 0)
+                sum += get_num(na, n - Integer.parseInt(na[i]));
+            if (n - Integer.parseInt(na[i]) < 0)
+                break;
+        }
+        return sum;
     }
 
 
     public static void main(String[] args) {
 
-
-        System.out.println(solution("uak areuok"));
+        System.out.println(solution("1,2,3 4"));
 
 
     }
+
 }
