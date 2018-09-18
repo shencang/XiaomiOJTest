fb715c473d67c501374ddfa8c8d80367e5b8f5c3
shencang <2630610009@qq.com>
2018-09-18 21:57:12 +0800

    补充题目，readme

--- src/N11构建短字符串.java
+++ src/N11构建短字符串.java
@@ -1,3 +1,26 @@
+/*
+描述
+给定任意一个较短的子串，和另一个较长的字符串，判断短的字符串是否能够由长字符串中的字符组合出来，且长串中的每个字符只能用一次。
+
+输入
+一行数据包括一个较短的字符串和一个较长的字符串，用一个空格分隔，如： ab aab bb abc aa cccc uak areuok
+
+输出
+如果短的字符串可以由长字符串中的字符组合出来，返回字符串 “true”，否则返回字符串 "false"，注意返回字符串类型而不是布尔型。
+
+输入样例
+a b
+aa ab
+aa aab
+uak areuok
+
+
+输出样例
+false
+false
+true
+true
+ */
 public class N11构建短字符串 {
 
     private static String solution(String line) {
@@ -34,7 +57,6 @@ public class N11构建短字符串 {
         }
         return true;
     }
-
 
 
     public static void main(String[] args) {
