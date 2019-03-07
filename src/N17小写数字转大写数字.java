ceccfbfb1c465eda4ef90bcf77415691774c30c6
shencang <2630610009@qq.com>
2019-03-07 19:00:36 +0800

    N17测试

---
+++ src/N17小写数字转大写数字.java
@@ -0,0 +1,45 @@
+ import  java.util.*;
+
+public class N17小写数字转大写数字 {
+    /**
+     * 序号：#17
+     * 难度：有挑战
+     * 时间限制：1000ms
+     * 内存限制：10M
+     * 描述
+     * 实现一个算法，可以将小写数字转换成大写数字。
+     * 输入
+     *
+     * 输入一个整数。范围在0～450亿之间。
+     *
+     * 输出
+     *
+     * 输出对应的大写数字，以“元整”结尾。 大写数字要符合汉语读写习惯。
+     *
+     * 输入样例
+     * 0
+     * 5
+     * 233
+     * 1001
+     * 40607
+     * 8900000000
+     *
+     * 输出样例
+     * 零元整
+     * 伍元整
+     * 贰佰叁拾叁元整
+     * 壹仟零壹元整
+     * 肆万零陆佰零柒元整
+     * 捌拾玖亿元整
+     */
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String line;
+        while (scan.hasNextLine()) {
+            line = scan.nextLine().trim();
+            // please write your code here
+
+            // System.out.println("answer");
+        }
+    }
+}
